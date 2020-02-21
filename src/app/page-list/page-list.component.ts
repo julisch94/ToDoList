6c5af0d6eaec0dc6e6880984f1bf6f9a5d658324
Julian Schmidt <julian.schmidt@nexusgroup.com>
2020-02-21 15:42:49 +0100

    add toggle for open and done

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-list',
-  templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.scss']
+    selector: 'app-page-list',
+    templateUrl: './page-list.component.html',
+    styleUrls: ['./page-list.component.scss']
 })
 export class PageListComponent implements OnInit {
 
-  constructor() { }
+    public showOpen = true;
+    public showDone = true;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
