8b9480fba3238660c77af10e2b60d67870d81f6e
Julian Schmidt <julian.schmidt@nexusgroup.com>
2020-02-21 16:05:49 +0100

    implement toggle mechanism for todos

--- src/app/_template/template-todo/template-todo.component.ts
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../_interface/todo';
 
 @Component({
-  selector: 'app-template-todo',
-  templateUrl: './template-todo.component.html',
-  styleUrls: ['./template-todo.component.scss']
+    selector: 'app-template-todo',
+    templateUrl: './template-todo.component.html',
+    styleUrls: ['./template-todo.component.scss']
 })
 export class TemplateTodoComponent implements OnInit {
 
-  constructor() { }
+    public todo: Todo;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.todo = {
+            id: 1,
+            text: "Buy groceries",
+            done: false,
+            position: 0,
+        };
+    }
+
+    ngOnInit() {
+    }
+
+    public changeCheck(event?: any): void {
+        this.todo.done = !this.todo.done;
+        console.log(`Todo at position ${this.todo.position} was changed to ${this.todo.done}`);
+    }
 
 }
